c249bc45c81909ff121076cd81f286e829ad4666
Miguel Neves <miguelneves@ua.pt>
2021-01-26 19:33:14 +0000

    Added gallery fragment (empty) + taking and (locally) saving a picture
    +InfoFragment empty file

---
+++ ZooExplorer/app/src/main/java/com/cm/zooexplorer/InfoFragment.java
@@ -0,0 +1,30 @@
+package com.cm.zooexplorer;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Contains the habitat information view, a list of info about the habitat's species.
+ */
+public class InfoFragment extends Fragment {
+
+    public InfoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_info, container, false);
+        return rootView;
+    }
+}
